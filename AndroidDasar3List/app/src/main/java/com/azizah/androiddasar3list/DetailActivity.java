e03be48126180f4b3763c8d21186c71f86506f07
azizah0408 <na179148@gmail.com>
2019-06-21 19:59:25 +0700

    folder5

---
+++ AndroidDasar3List/app/src/main/java/com/azizah/androiddasar3list/DetailActivity.java
@@ -0,0 +1,29 @@
+package com.azizah.androiddasar3list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    private static final String TAG = "DetailActivity";
+    TextView tvnamaBuah;
+    ImageView ivgambarbuah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        String namabuah = getIntent().getStringExtra(Konstanta.DATANAMA);
+        int gambarbuah = getIntent().getIntExtra(Konstanta.DATAGAMBAR,0);
+        Log.d(TAG, "Nama: "+namabuah);
+        Log.d(TAG, "Gambar: "+gambarbuah);
+
+        tvnamaBuah = findViewById(R.id.tv_detail_nama);
+        ivgambarbuah = findViewById(R.id.iv_detail_gambar);
+
+        tvnamaBuah.setText(namabuah);
+        ivgambarbuah.setImageResource(gambarbuah);
+    }
+}
